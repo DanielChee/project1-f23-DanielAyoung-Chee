883b65534129cf207d406077978d7527b2f17d8e
Daniel Ayoung-Chee <dayoungchee3@gatech.edu>
2023-09-27 20:30:42 -0400

    accessing API functionality as well as increment/decrement buttons

--- index.js
+++ index.js
@@ -4,17 +4,50 @@ async function fetchData() {
     const BASE_URL = `https://pokeapi.co/api/v2/pokemon/${index}/`;
     const response = await fetch(BASE_URL); //fetching info from URL
     const data = await response.json(); //converting to json
-    const name = data.name;
-    console.log(response);
 
-    const nameDiv = document.getElementById("name");
-    const nameHTML = `
-    <p>Name: ${name}</p>
-    `;
-    nameDiv.innerHTML = nameHTML;
+    const name = data.name;
+    const image = data.image;
+    const height = data.height;
+    const weight = data.weight;
+    const hp = data.stats[0].base_stat;
+    const attack = data.stats[1].base_stat;
+    const defense = data.stats[2].base_stat;
+    const specialAttack = data.stats[3].base_stat;
+    const specialDefense = data.stats[4].base_stat;
+    const speed = data.stats[5].base_stat;
+    const movesArray = data.moves;
+    const moveNames = [];
+    for (const move of movesArray) {
+        moveNames.push(move.move.name);
+    }
+
+    
+    document.getElementById("name-pokemon").innerHTML = `${name}`;
+    document.getElementById("height").innerHTML = `${height}`;
+    document.getElementById("weight").innerHTML = `${weight}`;
+    document.getElementById("hp").innerHTML = `${hp}`;
+    document.getElementById("attack").innerHTML = `${attack}`;
+    document.getElementById("defense").innerHTML = `${defense}`;
+    document.getElementById("special-attack").innerHTML = `${specialAttack}`;
+    document.getElementById("special-defense").innerHTML = `${specialDefense}`;
+    document.getElementById("speed").innerHTML = `${speed}`;
+    document.getElementById('img-pokemon').src = data.sprites.front_default;
 }
 
-document.addEventListener('click', () => {
-    fetchData();
-    index++;
+fetchData();
+
+document.addEventListener('click', (event) => {
+    const button = document.getElementById("decrement");
+    if (event.target === button && index > 1) {
+        index--;
+        fetchData();
+    }
+});
+
+document.addEventListener('click', (event) => {
+    const button = document.getElementById("increment");
+    if (event.target === button && index < 1017) {
+        index++;
+        fetchData();
+    }
 });
